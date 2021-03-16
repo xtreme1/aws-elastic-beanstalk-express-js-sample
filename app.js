cd939272b51553d4612746fa5dd44cf0730ecf04
Swartchick <xtreme1@cox.net>
2021-03-16 04:10:38 -0700

    change to nicer greeting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello You rat bastard!'));
+app.get('/', (req, res) => res.send('Good Morning my friend'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
